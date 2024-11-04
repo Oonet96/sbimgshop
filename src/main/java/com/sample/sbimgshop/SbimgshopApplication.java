6fdb32b14dc8c42e90fb5cf31b6b554609017208
Oonet96 <captaing96@gmail.com>
2024-11-04 11:38:42 +0900

    24.11.04 HT : git init

---
+++ src/main/java/com/sample/sbimgshop/SbimgshopApplication.java
@@ -0,0 +1,13 @@
+package com.sample.sbimgshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbimgshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbimgshopApplication.class, args);
+	}
+
+}
